7a0af14fce29369b80f006316b2f8a10a3fe7597
Anh Tuan <thongocnguyen1011@gmail.com>
2023-11-21 09:41:39 +0700

    starbucks: use js to create main

---
+++ starbuck-clone/data/home/noteContent.js
@@ -0,0 +1,9 @@
+const htmlNote = `
+  <div class="note">
+    *At participating stores. Some restrictions apply. See
+    <span class="rewards">https://www.starbucks.com/rewards</span>
+    for details.
+  </div>
+`;
+
+export default htmlNote;
---
+++ starbuck-clone/data/home/primaryContent.js
@@ -0,0 +1,58 @@
+const primaryContent = [{
+  id: "1",
+  image: {
+    alt: "A hand holding a smartphone with the Starbucks® mobile app open mobile ordering a Peppermint Mocha on a multi-colored background.",
+    src: "https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-87320.jpg"
+  },
+  title: "Festivity at your fingertips",
+  content: "Make the season even brighter when you join Starbucks® Rewards. You’ll save time with easy mobile ordering and earn free favorites.*",
+  "access-button": "Join for free",
+  size: "small",
+  reverse: "no"
+}, {
+  id: "2",
+  image: {
+    alt: "Two hot espresso drinks with whipped cream and an iced espresso drink in holiday-themed to-go cups.",
+    src: "https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-87323.png"
+  },
+  title: "Let’s celebrate",
+  content: "Your holi-yay favorites are here. Pick up a Peppermint Mocha, Iced Sugar Cookie Almondmilk Latte or Caramel Brulée Latte.",
+  "access-button": "Find your favourie",
+  size: "big",
+  reverse: "yes"
+}, {
+  id: "3",
+  image: {
+    alt: "Creamy iced chai drink with a foamy topping in a clear to-go cup.",
+    src: "https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-87325.png"
+  },
+  title: "Gingerbread meets chai",
+  content: "Say hello to the festive new Iced Gingerbread Oatmilk Chai. It's the beloved flavor of gingerbread with a twist. Here for a limited time.",
+  "access-button": "Order now",
+  size: "big",
+  reverse: "no"
+}, {
+  id: "4",
+  image: {
+    alt: "Sliced loaf with white icing.",
+    src: "https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-87328.jpg"
+  },
+  title: "Nicely spiced",
+  content: "Our Gingerbread Loaf with citrusy icing pairs deliciously with coffee.",
+  "access-button": "Order now",
+  size: "big",
+  reverse: "yes"
+}, {
+  id: "5",
+  image: {
+    alt: "Red coffee bag.",
+    src: "https://content-prod-live.cert.starbucks.com/binary/v2/asset/137-87330.jpg"
+  },
+  title: "Brew a cup of magic",
+  content: "The blends you love are back, including Starbucks® Christmas Blend 2023 with rare aged Sumatra.",
+  "access-button": "Order now",
+  size: "big",
+  reverse: "no"
+}];
+
+export default primaryContent;
