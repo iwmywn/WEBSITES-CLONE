8cc2adf96a1f4ec0ddd131ab8516b0f59ca06c97
Anh Tuan <thongocnguyen1011@gmail.com>
2023-12-29 09:51:39 +0700

    youtube: search bar (js)

--- youtube-clone/scripts/index.js
+++ youtube-clone/scripts/index.js
@@ -1,3 +1,14 @@
 import renderVideo from "./renderVideo.js";
 
 renderVideo();
+
+const searchContainerEle = document.querySelector('.js-search-bar');
+const searchBarEle = document.querySelector('.js-search-bar > input');
+
+searchBarEle.addEventListener('focus', () => {
+  searchContainerEle.classList.add('focus');
+});
+
+searchBarEle.addEventListener('blur', () => {
+  searchContainerEle.classList.remove('focus');
+});
